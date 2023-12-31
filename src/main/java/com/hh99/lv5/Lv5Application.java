242a04c987485b5e7168d4a05600b832c4d6e876
SeungbaeLee <aha224610@gmail.com>
2024-01-01 08:48:07 +0900

    Initial commit

---
+++ src/main/java/com/hh99/lv5/Lv5Application.java
@@ -0,0 +1,13 @@
+package com.hh99.lv5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lv5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lv5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hh99/lv5/Lv5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hh99.lv5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lv5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
