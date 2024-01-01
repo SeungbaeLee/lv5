9125a57f4bdc929e07ef26330a67a38e85cd865b
SeungbaeLee <aha224610@gmail.com>
2024-01-02 01:47:38 +0900

    feat : memberDto

---
+++ src/main/java/com/hh99/lv5/domain/member/dto/MemberPostDto.java
@@ -0,0 +1,43 @@
+package com.hh99.lv5.domain.member.dto;
+
+import com.hh99.lv5.domain.member.entity.Member;
+import com.hh99.lv5.domain.member.entity.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Getter
+@NoArgsConstructor
+public class MemberPostDto {
+
+    @Email
+    @NonNull
+    private String email;
+    @Pattern(
+            regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,15}$",
+            message = "비밀번호는 알파벳 대소문자, 숫자, 특수문자를 모두 포함해야 합니다."
+    )
+    @NonNull
+    private String password;
+    @NonNull
+    private String gender;
+    @NonNull
+    private String phoneNumber;
+    @NonNull
+    private String address;
+    @NonNull
+    private Role role;
+
+    public Member toEntity() {
+        return Member.builder()
+                .email(this.email)
+                .password(this.password)
+                .gender(this.gender)
+                .phoneNumber(this.phoneNumber)
+                .address(this.address)
+                .role(this.role)
+                .build();
+    }
+}
---
+++ src/main/java/com/hh99/lv5/domain/member/dto/MemberResponseDto.java
@@ -0,0 +1,41 @@
+package com.hh99.lv5.domain.member.dto;
+
+import com.hh99.lv5.domain.member.entity.Member;
+import com.hh99.lv5.domain.member.entity.Role;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class MemberResponseDto {
+
+    private Long memberId;
+    private String email;
+    private String gender;
+    private String phoneNumber;
+    private String address;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Builder
+    public MemberResponseDto(Long memberId, String email, String gender, String phoneNumber, String address, Role role) {
+        this.memberId = memberId;
+        this.email = email;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.role = role;
+    }
+
+    public static MemberResponseDto fromEntity(Member member) {
+        return MemberResponseDto.builder()
+                .memberId(member.getMemberId())
+                .email(member.getEmail())
+                .gender(member.getGender())
+                .phoneNumber(member.getPhoneNumber())
+                .address(member.getAddress())
+                .role(member.getRole())
+                .build();
+    }
+}
