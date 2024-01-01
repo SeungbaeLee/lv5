037e6f5bf1b69817c8b0b85c44f5afbaead70d97
SeungbaeLee <aha224610@gmail.com>
2024-01-02 05:39:36 +0900

    feat : repository 추가

---
+++ src/main/java/com/hh99/lv5/domain/bucket/repository/BucketRepository.java
@@ -0,0 +1,7 @@
+package com.hh99.lv5.domain.bucket.repository;
+
+import com.hh99.lv5.domain.bucket.entity.Bucket;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BucketRepository extends JpaRepository<Bucket, Long> {
+}
---
+++ src/main/java/com/hh99/lv5/domain/bucketProduct/repository/BucketProductRepository.java
@@ -0,0 +1,7 @@
+package com.hh99.lv5.domain.bucketProduct.repository;
+
+import com.hh99.lv5.domain.bucketProduct.entity.BucketProduct;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BucketProductRepository extends JpaRepository<BucketProduct, Long> {
+}
